6b73bac3664c20c0f529df348ffc99ea3f36b3c5
Steven Smith <github@icodeforyou.com>
2020-05-23 23:21:18 -0400

    better error message

--- lib/flutter_date_time_popover.dart
+++ lib/flutter_date_time_popover.dart
@@ -7,12 +7,12 @@ export 'date_time_picker/input_widget/date_time_input_widget.dart';
 
 String formattedDate(DateTime dateTime) {
   assert(dateTime != null, 'Cannot pass null dateTime');
-  if (dateTime == null) throw error('Cannot pass null dateTime');
+  if (dateTime == null) throw error('Cannot pass "null" dateTime to "formattedTime(dateTime)"');
   return DateFormat('EEE, MMM d, yyyy').format(dateTime);
 }
 
 String formattedTime(DateTime dateTime) {
   assert(dateTime != null, 'Cannot pass null dateTime');
-  if (dateTime == null) throw error('Cannot pass null dateTime');
+  if (dateTime == null) throw error('Cannot pass "null" dateTime to "formattedTime(dateTime)"');
   return DateFormat('h:mm:ss a').format(dateTime);
 }
